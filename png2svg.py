abeb432f16c0051179b871b1aa123aca00fd1615
CrossDarkrix <60906986+CrossDarkrix@users.noreply.github.com>
2022-03-11 21:57:14 +0900

    code optimisation.

--- png2svg.py
+++ png2svg.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
  
-import logging, sys, operator
+import sys, operator
 from collections import deque
 from io import StringIO
 from PIL import Image
-
-logging.basicConfig()
-log = logging.getLogger('png2svg')
 
 def add_tuple(a, b):
     return tuple(map(operator.add, a, b))
@@ -27,7 +24,7 @@ def magnitude(a):
 def normalize(a):
     mag = magnitude(a)
     assert mag > 0, "Cannot normalize a zero-length vector"
-    return tuple(map(operator.floordiv, a, [mag]*len(a)))
+    return tuple(map(operator.truediv, a, [mag]*len(a)))
 
 def svg_header(width, height):
     return """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
@@ -88,118 +85,14 @@ def joined_edges(assorted_edges, keep_every_point=False):
             raise Exception("Failed to find connecting edge")
     return pieces
 
-def rgba_image_to_svg_contiguous(im, opaque=None, keep_every_point=False):
-
-    # collect contiguous pixel groups
-
-    adjacent = ((1, 0), (0, 1), (-1, 0), (0, -1))
-    visited = Image.new("1", im.size, 0)
-
-    color_pixel_lists = {}
-
-    width, height = im.size
-    for x in range(width):
-        for y in range(height):
-            here = (x, y)
-            if visited.getpixel(here):
-                continue
-            rgba = im.getpixel((x, y))
-            if opaque and not rgba[3]:
-                continue
-            piece = []
-            queue = [here]
-            visited.putpixel(here, 1)
-            while queue:
-                here = queue.pop()
-                for offset in adjacent:
-                    neighbour = add_tuple(here, offset)
-                    if not (0 <= neighbour[0] < width) or not (0 <= neighbour[1] < height):
-                        continue
-                    if visited.getpixel(neighbour):
-                        continue
-                    neighbour_rgba = im.getpixel(neighbour)
-                    if neighbour_rgba != rgba:
-                        continue
-                    queue.append(neighbour)
-                    visited.putpixel(neighbour, 1)
-                piece.append(here)
-
-            if not rgba in color_pixel_lists:
-                color_pixel_lists[rgba] = []
-            color_pixel_lists[rgba].append(piece)
-
-    del adjacent
-    del visited
-
-    # calculate clockwise edges of pixel groups
-
-    edges = {
-        (-1, 0):((0, 0), (0, 1)),
-        (0, 1):((0, 1), (1, 1)),
-        (1, 0):((1, 1), (1, 0)),
-        (0, -1):((1, 0), (0, 0)),
-        }
-
-    color_edge_lists = {}
-
-    for rgba, pieces in list(color_pixel_lists.items()):
-        for piece_pixel_list in pieces:
-            edge_set = set([])
-            for coord in piece_pixel_list:
-                for offset, (start_offset, end_offset) in list(edges.items()):
-                    neighbour = add_tuple(coord, offset)
-                    start = add_tuple(coord, start_offset)
-                    end = add_tuple(coord, end_offset)
-                    edge = (start, end)
-                    if neighbour in piece_pixel_list:
-                        continue
-                    edge_set.add(edge)
-            if not rgba in color_edge_lists:
-                color_edge_lists[rgba] = []
-            color_edge_lists[rgba].append(edge_set)
-
-    del color_pixel_lists
-    del edges
-
-    # join edges of pixel groups
-
-    color_joined_pieces = {}
-
-    for color, pieces in list(color_edge_lists.items()):
-        color_joined_pieces[color] = []
-        for assorted_edges in pieces:
-            color_joined_pieces[color].append(joined_edges(assorted_edges, keep_every_point))
-
-    s = StringIO()
-    s.write(svg_header(*im.size))
-
-    for color, shapes in list(color_joined_pieces.items()):
-        for shape in shapes:
-            s.write(""" <path d=" """)
-            for sub_shape in shape:
-                here = sub_shape.pop(0)[0]
-                s.write(""" M %d,%d """ % here)
-                for edge in sub_shape:
-                    here = edge[0]
-                    s.write(""" L %d,%d """ % here)
-                s.write(""" Z """)
-            s.write(""" " style="fill:rgb%s; fill-opacity:%.3f; stroke:none;" />\n""" % (color[0:3], float(color[3]) / 255))
-
-    s.write("""</svg>\n""")
-    return s.getvalue()
-
-def png_to_svg(filename, contiguous=None, opaque=None, keep_every_point=None):
+def png_to_svg(filename):
     try:
-        im = Image.open(filename)
+        im_rgba = Image.open(filename).convert('RGBA')
     except IOError as e:
         sys.stderr.write('%s: Could not open as image file\n' % filename)
         sys.exit(1)
-    im_rgba = im.convert('RGBA')
 
-    if contiguous:
-        return rgba_image_to_svg_contiguous(im_rgba, opaque, keep_every_point)
-    else:
-        return rgba_image_to_svg_pixels(im_rgba, opaque)
+    return rgba_image_to_svg_pixels(im_rgba, None)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
